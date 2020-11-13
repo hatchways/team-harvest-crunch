ab283abdd717ac75a0c23b99b3732d271ee2c61f
Tushar Khunt <tusharkhunt121@gmail.com>
2020-11-13 15:33:06 -0500

    added register & login pages

--- client/src/pages/NavBar.js
+++ client/src/pages/NavBar.js
@@ -1,39 +1,50 @@
 import React from "react";
-import { makeStyles } from '@material-ui/core/styles';
-import { AppBar, Toolbar, IconButton, Typography, Button } from "@material-ui/core";
-import MenuIcon from '@material-ui/icons/Menu';
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Button,
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
 
 const useStyles = makeStyles((theme) => ({
-    rootHeader: {
-      flexGrow: 1,
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    },
+  rootHeader: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
 }));
 
 export default function Navbar() {
-    const classes = useStyles();
-    return(
-        <div className={classes.rootHeader}>
-            <AppBar position="static">
-                <Toolbar>
-                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                    <MenuIcon />
-                </IconButton>
-                <Typography variant="h6" className={classes.title}>
-                    HARVEST CRUNCH BAKERY
-                </Typography>
-                <Button color="inherit">SHOP</Button>
-                <Button color="inherit">MESSAGES</Button>
-                <Button color="inherit">MY FAVOURITES</Button>
-                <Button color="inherit">MY SHOP</Button>
-                <Button color="inherit">MY ACCOUNT</Button>
-                </Toolbar>
-            </AppBar>
-        </div>
-    );
+  const classes = useStyles();
+  return (
+    <div className={classes.rootHeader}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            HARVEST CRUNCH BAKERY
+          </Typography>
+          <Button color="inherit">SHOP</Button>
+          <Button color="inherit">MESSAGES</Button>
+          <Button color="inherit">MY FAVOURITES</Button>
+          <Button color="inherit">MY SHOP</Button>
+          <Button color="inherit">MY ACCOUNT</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
