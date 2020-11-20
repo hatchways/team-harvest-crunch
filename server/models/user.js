5af08d2e977151b45ee009bfdc6b7a710df23974
Tushar Khunt <tusharkhunt121@gmail.com>
2020-11-20 05:38:26 -0500

    changes info has been added in PR description

--- server/models/user.js
+++ server/models/user.js
@@ -1,27 +1,40 @@
 const mongoose = require("mongoose");
 
 const UserSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: false,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: false
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    shopName: {
+        type: String,
+        default: "Please Add Your Shop Name"
+    },
+    shopDescription: {
+        type: String,
+        default: "Please Add Your Shop Information"
+    },
+    shopCoverPic: {
+        type: String,
+        default:
+            "https://harvestcrunch-bakedgoods.s3-us-east-2.amazonaws.com/5fac90ecf662c13aa4730775/img7.png"
+    }
 });
 
 module.exports = mongoose.model("user", UserSchema);
