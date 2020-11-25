310e902029baa1185826af599343775274589e6a
austinkwok1997 <austin.kwok@protonmail.com>
2020-11-25 03:35:57 -0800

    added payment backend

---
+++ server/models/payment.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const PaymentSchema = mongoose.Schema({
+    productId: {
+        type: String,
+        required: true,
+    },
+    buyerId: {
+        type: String,
+        required: true,
+    },
+    sellerId: {
+        type: String,
+        required: true,
+    },
+    sessionId: {
+        type: String,
+        required: true,
+    },
+    completePurchase: {
+        type: Boolean,
+        default: false,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = mongoose.model("payment", PaymentSchema);
