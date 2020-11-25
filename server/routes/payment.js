1f7e12d55f01772ada0b6bab4613b77f0f13456c
austinkwok1997 <austin.kwok@protonmail.com>
2020-11-25 02:22:16 -0800

    fixed typo

--- server/routes/payment.js
+++ server/routes/payment.js
@@ -42,7 +42,7 @@ router.post(
       //     },
       //   },
       success_url: "http://127.0.0.1:3000/product/" + product_id,
-      cancel_url: "http://localhost:3000/product/" + product_id,
+      cancel_url: "http://127.0.0.1:3000/product/" + product_id,
     });
     res.json(session);
   }
