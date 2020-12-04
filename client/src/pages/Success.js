97932b7564c081d1185051fe15f957bb41ecead7
austinkwok1997 <austin.kwok@protonmail.com>
2020-12-03 18:18:32 -0800

    style changes to success page

--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "./NavBar";
-import { Container, Button, Box } from "@material-ui/core";
+import { Container, Button, Box, Card, CardContent } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(theme => ({
@@ -8,7 +8,8 @@ const useStyles = makeStyles(theme => ({
         color: "#4caf50",
         textAlign: "center",
         fontSize: "60px",
-        marginBottom: theme.spacing(1),
+        marginTop: theme.spacing(2),
+        marginBottom: theme.spacing(1)
     },
     rootH3: {
         textAlign: "center",
@@ -16,7 +17,14 @@ const useStyles = makeStyles(theme => ({
         marginTop: theme.spacing(1)
     },
     rootButton: {
-        marginTop: theme.spacing(6),
+        marginTop: theme.spacing(4),
+        marginLeft: theme.spacing(1),
+        marginRight: theme.spacing(1)
+    },
+    rootCard: {
+        marginTop: theme.spacing(5),
+        width: "800px",
+        margin: "auto"
     }
 }));
 
@@ -26,21 +34,34 @@ export default function Success() {
         <div>
             <Navbar />
             <Container className={classes.rootContainer}>
-                <h1 className={classes.rootH1}>Success!</h1>
-                <h3 className={classes.rootH3}>
-                    Thank you for making a purchase with us.
-                </h3>
-                <Box textAlign="center">
-                    <Button
-                        variant="contained"
-                        size="large"
-                        color="primary"
-                        className={classes.rootButton}
-                        href="/"
-                    >
-                        Return Home
-                    </Button>
-                </Box>
+                <Card className={classes.rootCard}>
+                    <CardContent>
+                        <h1 className={classes.rootH1}>Purchase Complete!</h1>
+                        <h3 className={classes.rootH3}>
+                            Thank you for shopping with us.
+                        </h3>
+                        <Box textAlign="center">
+                            <Button
+                                variant="contained"
+                                size="large"
+                                color="primary"
+                                className={classes.rootButton}
+                                href="/"
+                            >
+                                Return Home
+                            </Button>
+                            <Button
+                                variant="contained"
+                                size="large"
+                                color="primary"
+                                className={classes.rootButton}
+                                href="/purchase-history"
+                            >
+                                Purchase History
+                            </Button>
+                        </Box>
+                    </CardContent>
+                </Card>
             </Container>
         </div>
     );
